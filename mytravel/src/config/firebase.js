165b5c5fede054b75f7dcf548e79d03e8756d9ae
Pawel Lecki <p.lecki@mindz.it>
2019-02-03 16:27:37 +0100

    create react app, add firebase config

---
+++ mytravel/src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+    apiKey: "AIzaSyCFPHjvjA2kCJTVAPvg8v57FymM9NjnDlc",
+    authDomain: "mytravel-96d22.firebaseapp.com",
+    databaseURL: "https://mytravel-96d22.firebaseio.com",
+    projectId: "mytravel-96d22",
+    storageBucket: "mytravel-96d22.appspot.com",
+    messagingSenderId: "672178870039"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
