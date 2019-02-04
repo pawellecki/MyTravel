6235c1a951688545d833be9f817e81781309d136
Pawel Lecki <p.lecki@mindz.it>
2019-02-04 14:27:23 +0100

    add React.memo to Input

--- mytravel/src/App.js
+++ mytravel/src/App.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createProject } from './store/actions/projectActions'
-import Button from "./components/Form/Button/Button"
-import Input from "./components/Form/Input/Input"
-import './App.css';
+import Button from 'components/Form/Button/Button'
+import Input from './components/Form/Input/Input'
+import './App.css'
 
 class App extends Component {
+    state = {
+        title: '',
+        content: ''
+    }
 
-  state = {
-    title: '',
-    content: ''
-  }
+    handleChange = (event, fieldName) => {
+        this.setState({
+            [fieldName]: event.target.value
+        })
+    }
 
-  handleChange = (event, fieldName) => {
-    this.setState({
-      [fieldName]: event.target.value
-    })
-  }
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.createProject(this.state)
+    }
 
-  handleSubmit = event => {
-    e.preventDefault();
-    this.props.createProject(this.state);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <h5>Create a New Project</h5>
-          <div>
-            <Input
-              onChange={this.handleChange} 
-              name='title'
-              label="The title"
-            />
-            {/* <input type="text" id='title' onChange={this.handleChange} />
-            <label htmlFor="title">Project Title</label> */}
-          </div>
-          <div>
-            <input type="text" id='content' onChange={this.handleChange} />
-            <label htmlFor="title">Project content</label>
-          </div>
-          <Button title='Create project' />
-        </form>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.handleSubmit}>
+                    <h5>Create a New Project</h5>
+                    <Input
+                        onChange={this.handleChange}
+                        name="title"
+                        label="The title"
+                    />
+                    <Input
+                        onChange={this.handleChange}
+                        name="content"
+                        label="The content"
+                    />
+                    <Button title="Create project" />
+                </form>
+            </div>
+        )
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    createProject: project => dispatch(createProject(project))
-  }
+    return {
+        createProject: project => dispatch(createProject(project))
+    }
 }
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(
+    null,
+    mapDispatchToProps
+)(App)
--- mytravel/src/components/Form/Input/Input.js
+++ mytravel/src/components/Form/Input/Input.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Input = ({type = 'text', name, onChange, label}) => (
-    <>
+const Input = React.memo( ({type = 'text', name, onChange, label})=> (
+    <div>
         <input 
             type={type}
             onChange={event  => onChange(event, name)} 
             id={name}
         />
         <label htmlFor={name}>{label}</label>
-    </>
-)
+    </div>
+))
 
 export default Input
