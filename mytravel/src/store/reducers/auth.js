49a999123e263559f92c1f59cfa51f8ec82ab824
Pawel Lecki <p.lecki@mindz.it>
2019-02-13 11:02:14 +0100

    add singUp success and error reducer

--- mytravel/src/store/reducers/auth.js
+++ mytravel/src/store/reducers/auth.js
@@ -25,6 +25,23 @@ const auth = (state = initState, action) => {
 
         default:
             return state
+
+        case 'SIGNUP_SUCCESS':
+            console.log("signUP success!!")
+            return {
+                ...state,
+                authError: null
+            }
+
+        case 'SIGNUP_ERROR':
+            console.log("signUP error ...")
+            return {
+                ...state,
+                authError: action.err.message
+            }
+
+        default:
+            return state
     }
 }
 
