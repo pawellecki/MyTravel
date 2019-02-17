f921991f42b10e1eb8fabacdb5e7bf9a9b907165
Pawel Lecki <p.lecki@mindz.it>
2019-02-17 12:07:37 +0100

    add file upload button

--- mytravel/src/components/ImageUpload/ImageUpload.js
+++ mytravel/src/components/ImageUpload/ImageUpload.js
@@ -3,18 +3,34 @@ import React, { Component } from 'react'
 import Button from '../../components/Form/Button/Button'
 import Input from '../../components/Form/Input/Input'
 
-
-import styles from './LogIn.module.css'
+import styles from './ImageUpload.module.css'
 
 class ImageUpload extends Component {
+
+    state = {
+        image: null,
+        imageUrl: ''
+    }
+
     render() {
         // const {  } = this.props
 
         return (
             <div className={styles.root}>
-                
+                <input type="file" onChange={this.handleChange} />
+                <button>uploaddd</button>
             </div>
         )
+    }
+
+    handleChange = e => {
+        const image = e.target.files[0]
+        console.log('test:',image)
+        if (image) {
+            this.setState(() => ({
+                image
+            }))
+        }
     }
 }
 
--- mytravel/src/pages/TravelCard/TravelCard.js
+++ mytravel/src/pages/TravelCard/TravelCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ImageUpload from '../../components/ImageUpload/ImageUpload'
 
 class TravelCard extends Component {
 
@@ -9,6 +10,7 @@ class TravelCard extends Component {
                 <div>{title}</div>
                 <div>{content}</div>
                 <div>{id}</div>
+                <ImageUpload />
             </div>
         )
     }
