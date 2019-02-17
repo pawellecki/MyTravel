f536344fdd26613b55059fbc135bab43d244d21b
Pawel Lecki <p.lecki@mindz.it>
2019-02-17 11:19:19 +0100

    add empty ImageUpload

--- mytravel/src/components/Form/Input/Input.js
+++ mytravel/src/components/Form/Input/Input.js
@@ -1,12 +1,9 @@
 import React from 'react'
+import styles from './Input.module.css'
 
-const Input = React.memo( ({type = 'text', name, onChange, label})=> (
-    <div>
-        <input 
-            type={type}
-            onChange={e  => onChange(e, name)} 
-            id={name}
-        />
+const Input = React.memo(({ type = 'text', name, onChange, label }) => (
+    <div className={styles.root}>
+        <input type={type} onChange={e => onChange(e, name)} id={name} />
         <label htmlFor={name}>{label}</label>
     </div>
 ))
